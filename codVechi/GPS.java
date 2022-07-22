005c52133c95d0f68fe17da7a5fa95f2b9870ae9
Tanase Dragos-Iulian <gandalfeluldemere@gmail.com>
2022-07-22 16:58:19 +0300

    Create GPS.java

---
+++ codVechi/GPS.java
@@ -0,0 +1,13 @@
+package com.example.mainapp;
+
+import android.location.Location;
+import android.location.LocationManager;
+import android.widget.TextView;
+
+public class GPS {
+    protected LocationManager locationManager;
+    protected TextView txtLat;
+    protected Location location;
+    protected boolean o = true;
+    GPS(){};
+}
