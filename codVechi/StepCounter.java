81e6a4aafe7714934008a3cb0516802494a81ff4
Tanase Dragos-Iulian <gandalfeluldemere@gmail.com>
2022-07-22 17:00:52 +0300

    Create StepCounter.java

---
+++ codVechi/StepCounter.java
@@ -0,0 +1,83 @@
+package com.example.mainapp;
+
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.widget.TextView;
+
+import java.io.File;
+import java.io.FileOutputStream;
+
+/*
+    public class MainActivity extends AppCompatActivity {
+    ///variables
+    private TextView textViewStepCounter,textViewStepCounterSensor;
+    private SensorManager sensorManager;
+    private Sensor mStepCounter;
+    int stepCount=0,checkStep=0;
+    Stepcounter step= new Stepcounter();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if(ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACTIVITY_RECOGNITION) == PackageManager.PERMISSION_DENIED){ //ask for permission
+            requestPermissions(new String[]{Manifest.permission.ACTIVITY_RECOGNITION}, 0);
+        }
+        if(ContextCompat.checkSelfPermission(this,
+                Manifest.permission.MANAGE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED){ //ask for permission
+            requestPermissions(new String[]{Manifest.permission.MANAGE_EXTERNAL_STORAGE}, 0);
+        }
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        textViewStepCounter=findViewById(R.id.textViewStepCounter);
+        textViewStepCounterSensor=findViewById(R.id.textViewStepCounterSensor);
+        step.sensorManager =(SensorManager) getSystemService(SENSOR_SERVICE);
+        step.mStepCounter=step.sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
+    }
+ */
+
+public class StepCounter {
+
+    protected SensorManager msensorManager;
+    protected Sensor mStepCounter;
+    protected TextView textViewStepCounter;
+
+
+
+    StepCounter(){};
+
+    protected SensorEventListener sensorEventListener = new SensorEventListener(){
+
+
+        @Override
+        public void onSensorChanged( SensorEvent sensorEvent) {
+            float stepCount = sensorEvent.values[0];
+            textViewStepCounter.setText("PASI: " + sensorEvent.values[0]);
+            String stepCSV = "\n" + String.valueOf(stepCount);
+
+
+            try {
+                File path = new File("/storage/emulated/0/Download");
+                File file = new File(path + "/step.csv");
+                FileOutputStream f = new FileOutputStream(file, true);
+                try {
+
+                    f.write(stepCSV.getBytes());
+
+                    f.flush();
+
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int i) {
+
+        }
+    };
+}
