df48f2cd3d82f2495b91cc611d5ba9307847fc9f
George Herman <108926965+nexxx2027@users.noreply.github.com>
2022-08-04 13:44:45 +0300

    Add files via upload

---
+++ PopUpWindow.java
@@ -0,0 +1,89 @@
+package com.example.uisimplu;
+
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatDialogFragment;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+
+public class PopUpWindow extends AppCompatDialogFragment{
+
+    private EditText editText;
+    protected String entry;
+    protected String data;
+    String once ="x_acc" + "," + "y_acc" + "," + "z_acc" + "," + "x_gyro" + "," + "y_gyro" + "," + "z_gyro" + "," + "mag_tesla" + "," + "wifi_rssi"+","+"denumire";
+
+
+    public Dialog onCreateDialog(Bundle savedInstanceState){
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+        View view = inflater.inflate(R.layout.layout_dialog,null);
+        builder.setView(view).setTitle("Save").setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i)
+            {
+                entry="";
+                storePP(false,once);
+                dialogInterface.dismiss();
+            }
+        }).setPositiveButton("Save", new DialogInterface.OnClickListener()
+        {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                entry = editText.getText().toString();
+                storePP(true,once);
+            }
+        });
+        editText = view.findViewById(R.id.edit_locatie);
+        return builder.create();
+    }
+    public void storePP(boolean state, String once)
+    {
+        if(state) {
+           String  entry=data+getEntry();
+            try {
+                File path = new File("/storage/emulated/0/Download");
+                File file = new File(path + "/PinPoints.csv");
+                FileOutputStream f = new FileOutputStream(file, true);
+                if (file.length() != 0) {
+                    try {
+                        f.write(entry.getBytes());
+                        f.flush();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                } else {
+                    try {
+                        f.write(once.getBytes());
+                        f.write(entry.getBytes());
+                        f.flush();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+        else if (state == false)
+        {
+
+        }
+    }
+    public String getEntry()
+    {
+        return entry;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+}
+
